1d7a7c604cbc837f96cf9db6e4d05adc70cd271d
futz <stefan.patatu@gmail.com>
2015-11-09 17:15:42 +0100

    no fuck!

--- Translato/WcfServiceLibrary/MODEL/Users.cs
+++ Translato/WcfServiceLibrary/MODEL/Users.cs
@@ -16,6 +16,5 @@ namespace WcfServiceLibrary.MODEL
         public string LastName { get; set; }
         public string Email { get; set; }
         public bool NewsletterOptOut { get; set; }
-        //fuuuuuckkkk!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     }
 }
