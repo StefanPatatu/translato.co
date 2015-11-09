ecedb16b1dd205c68c27d6add3c53035e7b0ce1f
futz <stefan.patatu@gmail.com>
2015-11-09 14:18:03 +0100

    merged provider and requester into one table

--- Translato/Database/dbo.Provider.sql
+++
@@ -1,7 +0,0 @@
-﻿CREATE TABLE [dbo].[Provider]
-(
-	[ProviderId] INT IDENTITY(1,1) PRIMARY KEY, 
-    [UserName] NVARCHAR(15) NOT NULL, 
-    [HashedPassword] NVARCHAR(100) NOT NULL, 
-    [PasswordSalt] NVARCHAR(15) NOT NULL
-)
---
+++ Translato/Database/dbo.User.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Provider]
+(
+	[UserId] INT IDENTITY(1,1) PRIMARY KEY, 
+    [UserName] NVARCHAR(15) NOT NULL, 
+    [HashedPassword] NVARCHAR(100) NOT NULL, 
+    [PasswordSalt] NVARCHAR(15) NOT NULL, 
+    [FirstName] NVARCHAR(20) NOT NULL, 
+    [LastName] NVARCHAR(20) NOT NULL, 
+    [Country] NVARCHAR(50) NOT NULL, 
+    [Email] NVARCHAR(50) NOT NULL, 
+    [EmailOptOut] BIT NOT NULL
+)
