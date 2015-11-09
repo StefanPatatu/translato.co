908d67179383f2a943dcda21dc3c7f93eebb4d67
futz <stefan.patatu@gmail.com>
2015-11-09 16:52:25 +0100

    last commit for today

--- Translato/Database/dbo.Languages.sql
+++ Translato/Database/dbo.Languages.sql
@@ -1,6 +1,6 @@
 ﻿CREATE TABLE [dbo].[Languages]
 (
-	[LanguageId] INT IDENTITY(1,1) NOT NULL, 
+	[LanguageId] INT IDENTITY(1,1) NOT NULL,
     [LanguageName] NVARCHAR(15) NOT NULL,
 	PRIMARY KEY ([LanguageId]),
 	CONSTRAINT UNIQUE_LANGUAGE UNIQUE([LanguageName])
--- Translato/Database/dbo.Users.sql
+++ Translato/Database/dbo.Users.sql
@@ -1,13 +1,13 @@
 ﻿CREATE TABLE [dbo].[Users]
 (
-	[UserId] INT IDENTITY(1,1) NOT NULL, 
-    [UserName] NVARCHAR(15) NOT NULL, 
-    [HashedPassword] NVARCHAR(100) NOT NULL, 
-    [PasswordSalt] NVARCHAR(15) NOT NULL, 
-    [FirstName] NVARCHAR(20) NOT NULL, 
-    [LastName] NVARCHAR(20) NOT NULL, 
-    [Email] NVARCHAR(50) NOT NULL, 
-    [NewsletterOptOut] BIT NOT NULL,
+	[UserId] INT IDENTITY(1,1) NOT NULL,
+    [UserName] NVARCHAR(15) NOT NULL,
+    [HashedPassword] NVARCHAR(100) NOT NULL,
+    [PasswordSalt] NVARCHAR(15) NOT NULL,
+    [FirstName] NVARCHAR(20) NOT NULL,
+    [LastName] NVARCHAR(20) NOT NULL,
+    [Email] NVARCHAR(50) NOT NULL,
+    [NewsletterOptOut] BIT NOT NULL DEFAULT 1,
 	PRIMARY KEY ([UserId]),
 	CONSTRAINT UNIQUE_USERNAME UNIQUE([UserName]),
 	CONSTRAINT UNIQUE_EMAIL UNIQUE([Email])
