2d277add102bc10c28fd49cd918386c0c278c6a5
Kristis133 <kristis133@hotmail.com>
2015-11-10 14:23:34 +0100

    Language class and LanguageTests.
    What am I even doing.

---
+++ Translato/WcfServiceLibrary.Tests/MODELTests/LanguageTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfServiceLibrary.MODEL;
+
+
+namespace WcfServiceLibrary.Tests.MODELTests
+{   [TestClass]
+    public class LanguageTests
+    {
+        [TestMethod]
+        //LAYER_Class_nameOfTheMethod_testedScenario_expectedBehaviour
+        public void MODEL_Language_PublicConstructor_CreateLanguage_LanguageIsCreated()
+        {
+            //arrange
+            int LanguageId = 1;
+            string LanguageName = "Lithuanian";
+
+            //act
+
+            Language language_m1 = new Language(
+                 LanguageId,
+                 LanguageName);
+
+            //assert
+            Assert.IsNotNull(language_m1);
+            Assert.AreEqual(1, language_m1.LanguageId, "Wrong UserId");
+            Assert.AreEqual("Lithuanian", language_m1.LanguageName, "Wrong LanguageName");
+        }
+        [TestMethod]
+        //LAYER_Class_nameOfTheMethod_testedScenario_expectedBehaviour
+        public void MODEL_Language_SetAndGetMethods_ModifyAllFieldsValues_AllValuesAreModified()
+        {
+            //arrange
+            int LanguageId = 1;
+            string LanguageName = "Lithuanian";
+            Language language_m2 = new Language(
+                LanguageId,
+                LanguageName);
+
+            //act
+            language_m2.LanguageId = 2;
+            language_m2.LanguageName = "not.Lithuanian";
+
+            //assert
+            Assert.IsNotNull(language_m2);
+            Assert.AreEqual(2, language_m2.LanguageId, "LanguageId not changed");
+            Assert.AreEqual("not.Lithuanian", language_m2.LanguageName, "LanguageName not changed");
+
+        }
+    }
+}
---
+++ Translato/WcfServiceLibrary/MODEL/Language.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfServiceLibrary.MODEL
+{
+    public class Language
+    {
+        private int p_LanguageId;
+        private string p_LanguageName;
+        private Language()
+        {
+
+        }
+        public Language(
+            int LanguageId,
+            string LanguageName)      {
+
+            this.LanguageId = LanguageId;
+            this.LanguageName = LanguageName;
+          
+        }
+        public int LanguageId
+        {
+            get { return p_LanguageId; }
+            set { p_LanguageId = value; }
+        }
+        public string LanguageName
+        {
+            get { return p_LanguageName; }
+            set { p_LanguageName = value; }
+        }
+       
+    }
+}
