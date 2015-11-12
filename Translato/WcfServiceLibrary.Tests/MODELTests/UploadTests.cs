503293d757555627c85a37939e2c1dd3394892b5
TheDarkSun <mwmwmw25@gmail.com>
2015-11-12 12:01:11 +0100

    Upload

---
+++ Translato/WcfServiceLibrary.Tests/MODELTests/UploadTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WcfServiceLibrary.MODEL;
+
+//author: DarkSun
+//helper: kool-kat
+
+namespace WcfServiceLibrary.Tests.MODELTests
+{
+    [TestClass]
+    public class UploadTests
+    {
+        [TestMethod]
+        //LAYER_Class_nameOfTheMethod_testedScenario_expectedBehaviour
+        public void MODEL_Upload_PublicConstructor_CreateUpload_UploadIsCreated()
+        {
+            //arrange
+            int UploadId = 5;
+            string Type = "Hey";
+            int TextId = 4;
+            int FileId = 8;
+
+            //act
+            Upload upload_m1 = new Upload(
+                UploadId,
+                Type,
+                TextId,
+                FileId);
+
+            //assert
+            Assert.IsNotNull(upload_m1, "Upload object is null");
+            Assert.AreEqual(5, upload_m1.UploadId, "Wrong UploadId");
+            Assert.AreEqual("Hey", upload_m1.Type, "Wrong Type");
+            Assert.AreEqual(4, upload_m1.TextId, "Wrong TextId");
+            Assert.AreEqual(8, upload_m1.FileId, "Wrong FileId");
+                }
+        [TestMethod]
+        //LAYER_Class_nameOfTheMethod_testedScenario_expectedBehaviour
+        public void MODEL_Upload_SetAndGetMethods_AttributesAreEdited_AllValuesUpdated ()
+        {
+            //arrange
+            int UploadId = 5;
+            string Type = "Hey";
+            int TextId = 4;
+            int FileId = 8;
+
+            Upload upload_m2 = new Upload(
+                UploadId,
+                Type,
+                TextId,
+                FileId);
+
+            //act
+            upload_m2.UploadId = 2;
+            upload_m2.Type = "LoL";
+            upload_m2.TextId = 1;
+            upload_m2.FileId = 5;
+
+            //assert
+            Assert.IsNotNull(upload_m2, "Upload object is null");
+            Assert.AreEqual(2, upload_m2.UploadId, "UploadId not changed");
+            Assert.AreEqual("LoL", upload_m2.Type, "Type not changed");
+            Assert.AreEqual(1, upload_m2.TextId, "TextId not changed");
+            Assert.AreEqual(5, upload_m2.FileId, "FileId not changed");
+        }
+    }
+}
---
+++ Translato/WcfServiceLibrary/MODEL/Upload.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//author: DarkSun
+//helped: kool-kat
+
+namespace WcfServiceLibrary.MODEL
+{
+    public class Upload
+    {
+        private int p_UploadId;
+        private string p_Type;
+        private int p_TextId;
+        private int p_FileId;
+
+        private Upload()
+        {
+
+        }
+
+        public Upload(
+            int UploadId,
+            string Type,
+            int TextId,
+            int FileId)
+        {
+            this.UploadId = UploadId;
+            this.Type = Type;
+            this.TextId = TextId;
+            this.FileId = FileId;
+        }
+
+        public int UploadId
+        {
+            get { return p_UploadId; }
+            set { p_UploadId = value; }
+        }
+        public string Type
+        {
+            get { return p_Type; }
+            set { p_Type = value; }
+        }
+        public int TextId
+        {
+            get { return p_TextId; }
+            set { p_TextId = value; }
+        }
+        public int FileId
+        {
+            get { return p_FileId; }
+            set { p_FileId = value; }
+        }
+    }
+}
