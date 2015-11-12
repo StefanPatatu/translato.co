3d578375768a344e1226001ccda6fc34072694b3
futz <stefan.patatu@gmail.com>
2015-11-12 14:36:46 +0100

    day end commit

--- Translato/Database/Properties/AssemblyInfo.cs
+++ Translato/Database/Properties/AssemblyInfo.cs
@@ -21,5 +21,5 @@
 //
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: System.Resources.NeutralResourcesLanguage("")]
+[assembly: System.Resources.NeutralResourcesLanguage("en-US")]
 
