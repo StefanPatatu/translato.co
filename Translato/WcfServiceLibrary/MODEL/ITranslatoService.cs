9525c37b545e1e15761122347a14cf6797102340
futz <stefan.patatu@gmail.com>
2015-11-19 10:26:49 +0100

    fixes

---
+++ Translato/WcfServiceLibrary/MODEL/ITranslatoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using WcfServiceLibrary.MODEL;
+
+namespace WcfServiceLibrary
+{
+    [ServiceContract]
+    public interface ITranslatoService
+    {
+        [OperationContract]
+        int InsertUser(User user);
+    }
+}
