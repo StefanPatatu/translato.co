1ff0173dc426adfd0bc7a40a53a84452833e38f6
Adrian Frunza <no@no.com>
2015-11-23 12:55:34 +0100

    CtrJob, CtrUpload, CtrSubmission

---
+++ Translato/WcfServiceLibrary/BLL/CtrJob.cs
@@ -0,0 +1,85 @@
+﻿//author: adrian
+//helpers:
+//last_checked:
+
+using System;
+using WcfServiceLibrary.MODEL;
+using WcfServiceLibrary.DAL;
+
+namespace WcfServiceLibrary.BLL
+{
+    public class CtrJob
+    {
+        //returns 1 if successful
+        //returns 0 if failure of any kind
+        public int insertJob(Job job)
+        {
+            int result = -1;
+
+            //validate JobName
+            if (
+                result == 0 ||
+                !Validate.isAlphaNumericWithUnderscore(job.jobName) ||
+                job.jobName.Equals(null) ||
+                !Validate.hasMaxLength(job.jobName, 50) ||
+                !Validate.hasMinLength(job.jobName, 1)
+              )
+            { result = 0; }
+
+            //validate DateCreated
+            if (
+                result == 0 ||
+                job.dateCreated.Equals(null)
+                )
+            { result = 0; }
+
+            //validate DurationInDays
+            if (
+                result == 0 ||
+                job.durationInDays<1 ||
+                job.durationInDays.Equals(null)
+                )
+            { result = 0; }
+
+            //validate LanguageFrom
+            if (
+                result == 0 ||
+                job.languageFrom.Equals(null)
+                )
+            { result = 0; }
+
+            //validate LanguageTo
+            if (
+                result == 0 ||
+                job.languageTo.Equals(null)
+                )
+            { result = 0; }
+
+            //validate User
+            if (
+                result == 0 ||
+                job.user.Equals(null)
+                )
+            { result = 0; }
+
+            //validate Upload
+            if (
+                result == 0 ||
+                job.upload.Equals(null)
+                )
+            { result = 0; }
+
+            if (result != 0)//safe to proceed
+            {
+                IJobs _DbJobs = new DbJobs();
+
+                result = _DbJobs.insertJob(job);
+            }
+            else
+            {
+                result = 0;
+            }
+            return result;
+        }
+    }
+}
---
+++ Translato/WcfServiceLibrary/BLL/CtrSubmission.cs
@@ -0,0 +1,67 @@
+﻿//author: adrian
+//helpers:
+//last_checked:
+
+using System;
+using WcfServiceLibrary.MODEL;
+using WcfServiceLibrary.DAL;
+
+namespace WcfServiceLibrary.BLL
+{
+    public class CtrSubmission
+    {
+        //returns 1 if successful
+        //returns 0 if failure of any kind
+        public int insertSubmission(Submission submission)
+        {
+            int result = -1;
+
+            //validate DateSubmitted
+            if (
+                result == 0 ||
+                submission.dateSubmitted.Equals(null)
+              )
+            { result = 0; }
+
+            //validate IsAwarded
+            if (
+                result == 0 ||
+                submission.isAwarded.Equals(null)
+                )
+            { result = 0; }
+
+            //validate UserId
+            if (
+                result == 0 ||
+                submission.user.Equals(null)
+                )
+            { result = 0; }
+
+            //validate UploadId
+            if (
+                result == 0 ||
+                submission.upload.Equals(null)
+                )
+            { result = 0; }
+
+            //validate JobId
+            if (
+                result == 0 ||
+                submission.job.Equals(null)
+                )
+            { result = 0; }
+
+            if (result != 0)//safe to proceed
+            {
+                ISubmissions _DbSubmissions = new DbSubmissions();
+
+                result = _DbSubmissions.insertSubmission(submission);
+            }
+            else
+            {
+                result = 0;
+            }
+            return result;
+        }
+    }
+}
