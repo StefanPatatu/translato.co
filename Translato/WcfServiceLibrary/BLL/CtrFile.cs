bf76d93a3b81aedfc936ea55dcb6c88468d81d13
Adrian Frunza <no@no.com>
2015-11-23 12:05:26 +0100

    FileCtr and FileCtrTests

---
+++ Translato/WcfServiceLibrary/BLL/CtrFile.cs
@@ -0,0 +1,34 @@
+﻿//author: adrian
+//helpers:
+//last_checked:
+
+using System;
+using WcfServiceLibrary.MODEL;
+using WcfServiceLibrary.DAL;
+
+namespace WcfServiceLibrary.BLL
+{
+    public class CtrFile
+    {
+        //returns 1 if successful
+        //returns 0 if failure of any kind
+        public int insertFile(File file)
+        {
+            int result = -1;
+
+           //validations to be added
+
+            if (result != 0)//safe to proceed
+            {
+                IFiles _DbFiles = new DbFiles();
+
+                result = _DbFiles.insertFile(file);
+            }
+            else
+            {
+                result = 0;
+            }
+            return result;
+        }
+    }
+}
